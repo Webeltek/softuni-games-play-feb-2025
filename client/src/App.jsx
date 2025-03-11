4e735a04327a6539eaca6c4d0a3f5163769a3b68
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2025-03-11 22:32:55 +0100

    Extract some components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
+import Header from './components/header/Header'
+import Home from './components/home/Home'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h1>Games Play</h1>
-    </>
+    <div id="box">
+
+      <Header />
+      <main id="main-content">
+        <Home />
+      </main>
+    </div>
   )
 }
 
