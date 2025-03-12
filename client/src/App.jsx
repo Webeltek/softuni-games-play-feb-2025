2543c9f864b537d19b155766abf66801b87b9721
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2025-03-12 17:30:25 +0100

    Lift email state and pass to comment create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,14 @@ import GameCatalog from './components/game-catalog/GameCatalog'
 import GameCreate from './components/game-create/GameCreate'
 import GameDetails from './components/game-details/GameDetails'
 import GameEdit from './components/game-edit/GameEdit'
+import { useState } from 'react'
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  const userLoginHandler = (email) => {
+    setEmail(email);
+  }
 
   return (
     <div id="box">
@@ -21,9 +27,9 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/games" element={ <GameCatalog />} />
           <Route path="/games/create" element={ <GameCreate />} />
-          <Route path="/games/:gameId/details" element={ <GameDetails />} />
+          <Route path="/games/:gameId/details" element={ <GameDetails email={email} />} />
           <Route path="/games/:gameId/edit" element={ <GameEdit />} />
-          <Route path="/login" element={ <Login />} />
+          <Route path="/login" element={ <Login onLogin={userLoginHandler}/>} />
           <Route path="/register" element={ <Register />} />
         </Routes>
       </main>
