bfb7d435bb170ec5dc77ffb29c6894bcb12c867c
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2025-03-12 11:59:20 +0100

    Get all games

--- client/src/components/game-catalog/GameCatalog.jsx
+++ client/src/components/game-catalog/GameCatalog.jsx
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react";
+import gameService from "../../services/gameService";
+
 export default function GameCatalog() {
+    const [games, setGames] = useState()
+
+    useEffect(()=>{
+        gameService.getAll()
+        .then(setGames)
+    }, [])
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
