21a75ad1ec322f868f56fdbd2af5787383191f09
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2025-03-19 00:21:57 +0100

    useContext for authData

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
 import { Routes, Route} from 'react-router'
+import { useState } from 'react'
+
+import { UserContext } from './contexts/UserContext';
 
 import Header from './components/header/Header'
 import Home from './components/home/Home'
@@ -9,7 +12,7 @@ import GameCatalog from './components/game-catalog/GameCatalog'
 import GameCreate from './components/game-create/GameCreate'
 import GameDetails from './components/game-details/GameDetails'
 import GameEdit from './components/game-edit/GameEdit'
-import { useState } from 'react'
+
 
 function App() {
   const [authData, setAuthData] = useState({});
@@ -19,21 +22,23 @@ function App() {
   }
 
   return (
-    <div id="box">
+    <UserContext.Provider value={ {...authData, userLoginHandler} }>
+      <div id="box">
 
-      <Header />
-      <main id="main-content">
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/games" element={ <GameCatalog />} />
-          <Route path="/games/create" element={ <GameCreate />} />
-          <Route path="/games/:gameId/details" element={ <GameDetails email={authData.email} />} />
-          <Route path="/games/:gameId/edit" element={ <GameEdit />} />
-          <Route path="/login" element={ <Login onLogin={userLoginHandler}/>} />
-          <Route path="/register" element={ <Register />} />
-        </Routes>
-      </main>
-    </div>
+        <Header />
+        <main id="main-content">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/games" element={ <GameCatalog />} />
+            <Route path="/games/create" element={ <GameCreate />} />
+            <Route path="/games/:gameId/details" element={ <GameDetails />} />
+            <Route path="/games/:gameId/edit" element={ <GameEdit />} />
+            <Route path="/login" element={ <Login />} />
+            <Route path="/register" element={ <Register />} />
+          </Routes>
+        </main>
+      </div>
+    </UserContext.Provider>
   )
 }
 
