f1417a28ed74a47e74da357ca4783bec27dc4f1a
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2025-03-25 16:55:29 +0100

    Auth Guards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,9 +28,12 @@ function App() {
           <Routes>
             <Route index element={<Home />} />
             <Route path="/games" element={ <GameCatalog />} />
-            <Route path="/games/create" element={ <GameCreate />} />
             <Route path="/games/:gameId/details" element={ <GameDetails />} />
-            <Route path="/games/:gameId/edit" element={ <GameEdit />} />
+            <Route  element={ <AuthGuard />} >
+              <Route path="/games/create" element={ <GameCreate />} />
+              <Route path="/games/:gameId/edit" element={ <GameEdit />} />
+              <Route path="/logout" element={ <Logout /> }  />
+            </Route>
             <Route path="/login" element={ <Login />} />
             <Route path="/register" element={ <Register />} />
           </Routes>
---
+++ client/src/components/guards/AuthGuard.jsx
@@ -0,0 +1,15 @@
+import { Navigate, Outlet } from "react-router";
+import useAuth from "../../hooks/useAuth";
+
+export default function AuthGuard() {
+    const { isAuthenticated } = useAuth();
+
+    if(!isAuthenticated){
+        return <Navigate to="/login" />
+    }
+    return (
+        <>
+            <Outlet />
+        </>
+    );
+}
